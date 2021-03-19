cf767f51cec155a7c7fddd2038c49728eb21322b
Thiago Ferreira <thiagology@users.noreply.github.com>
2021-03-19 04:29:03 -0300

    graphql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,36 @@
 import Card from "../components/Card";
 import { Container, Columns } from 'react-bulma-components';
+import { gql, useQuery } from '@apollo/client'; // para graphql
 
-const Index = () => <Container>
-  <Columns style={{marginTop: 4}}>
-    <Columns.Column>
-      <Card />
-    </Columns.Column>
-    <Columns.Column>
-      <Card />
-    </Columns.Column>
-  </Columns>
+const query = gql`
+  query{
+    launches{
+      id
+      mission_name
+      is_tentative
+      links {
+        mission_patch
+      }
+    }
+  }
+`;
 
-</Container>
+const Index = () => {
+  const { data, error, loading } = useQuery(query);
+
+  const launches = data?.launches ?? [] // null or undefined
+
+  return (
+    <Container>
+      <Columns style={{ marginTop: 40 }}>
+        {launches.map((launch) => (
+          <Columns.Column key={launch.id} size={4}>
+            <Card />
+          </Columns.Column>
+        ))}
+      </Columns>
+    </Container>
+  )
+}
 
 export default Index
